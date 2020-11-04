493ad80aeec78022e79374875faa5e02cf1e79e5
Dannah Jackson <70861209+djackson136@users.noreply.github.com>
2020-11-04 12:49:48 -0500

    Update TeamPractice.java

--- Team/src/TeamPractice.java
+++ Team/src/TeamPractice.java
@@ -3,9 +3,13 @@ public class TeamPractice {
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 7;
+		int c = 10;
+		double d = 7.5;
 		
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
