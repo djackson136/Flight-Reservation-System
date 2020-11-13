002f9fd8cc1d2173a9c4b4df41c9f3b60a6f4bfd
Dannah <Dannah@localhost>
2020-11-13 15:50:07 -0500

    loads Splash Screen and Main Menu

---
+++ Team/src/ProjectCode/Main.java
@@ -0,0 +1,28 @@
+package ProjectCode;
+
+import java.awt.EventQueue;
+
+public class Main {
+	public static void main(String[] args) {
+		SplashScreen splash = new SplashScreen();
+		splash.setVisible(true);
+		SplashScreen.showProgress(splash);
+					
+		
+		splash.setVisible(false);
+		splash.dispose();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow frame = new LoginWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+
+}
