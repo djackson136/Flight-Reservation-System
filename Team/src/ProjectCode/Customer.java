60732001b48911bd4c390bb806b97ab569c0177b
Dannah <Dannah@dannahs-mbp>
2020-12-02 13:39:40 -0500

    new Customer class

---
+++ Team/src/ProjectCode/Customer.java
@@ -0,0 +1,136 @@
+package ProjectCode;
+
+public class Customer {
+//extends User 
+	private String firstName;
+	private String lastName;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private int zipcode;
+	private String username;
+	private String password;
+	private String email;
+	private int ssn;
+	private String secQuestion;
+	private String secAnswer;
+	
+	public Customer() {
+		
+	}
+	
+	public Customer(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getSecQuestion() {
+		return secQuestion;
+	}
+
+	public void setSecQuestion(String secQuestion) {
+		this.secQuestion = secQuestion;
+	}
+
+	public String getSecAnswer() {
+		return secAnswer;
+	}
+
+	public void setSecAnswer(String secAnswer) {
+		this.secAnswer = secAnswer;
+	}
+	
+	// Customer Methods
+	public static void findFlight() {
+		
+	}
+	
+	public void addToAccount() {
+		
+	}
+	
+	
+	
+	
+	
+}
--- Team/src/ProjectCode/DbConnection.java
+++ Team/src/ProjectCode/DbConnection.java
@@ -10,8 +10,8 @@ public class DbConnection {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://35.202.73.103:3306/Customer";
-			String username = "root"; // "daiv"
-			String password = "teamdatabase"; // "project"
+			String username = "daiv"; // "daiv"
+			String password = "project"; // "project"
 			conn = DriverManager.getConnection(url, username, password);
 			return conn;
 		} catch (Exception e) {
@@ -23,7 +23,7 @@ public class DbConnection {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+		} 
 		return null;
 		
 	}
