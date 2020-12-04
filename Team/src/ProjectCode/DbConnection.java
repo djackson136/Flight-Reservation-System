c9cdd66c0837c5aacfb4b8d60c621dbf64da092a
Dannah <Dannah@dannahs-mbp>
2020-12-03 20:02:04 -0500

    updated Connection class

--- Team/src/ProjectCode/DbConnection.java
+++ Team/src/ProjectCode/DbConnection.java
@@ -2,42 +2,20 @@ package ProjectCode;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DbConnection {
 	public static Connection connect() {
-		Connection conn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://35.202.73.103:3306/Customer";
-			String username = "daiv"; // "daiv"
-			String password = "project"; // "project"
-<<<<<<< HEAD
-			conn = DriverManager.getConnection(url, username, password);
-=======
+			String username = "daiv";
+			String password = "project";
 			Connection conn = DriverManager.getConnection(url, username, password);
->>>>>>> branch 'master' of https://github.com/djackson136/Team-Project.git
+			
 			return conn;
 		} catch (Exception e) {
 			System.out.println("Connection Failed");
-<<<<<<< HEAD
-		} finally {
-			/*try {
-			
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-<<<<<<< HEAD
-			}
-=======
->>>>>>> branch 'master' of https://github.com/djackson136/Team-Project.git
-		} 
-=======
-			}*/
 		}
->>>>>>> branch 'master' of https://github.com/djackson136/Team-Project.git
 		return null;
-		
 	}
-
 }
