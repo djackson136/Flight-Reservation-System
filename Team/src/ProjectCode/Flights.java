e9b3612fed76fc491ba3b10ba51d393655c6edc6
Dannah <Dannah@dannahs-mbp>
2020-12-13 18:51:22 -0500

    Flights window

---
+++ Team/src/ProjectCode/Flights.java
@@ -0,0 +1,68 @@
+package ProjectCode;
+
+public class Flights {
+	private int flightID;
+	private int capacity;
+	private String DepCity;
+	private String ArrCity;
+	private String DepDate;
+	private String DepTime;
+	
+	public Flights() {
+		
+	}
+	
+
+	public int getFlightID() {
+		return flightID;
+	}
+
+
+	public void setFlightID(int flightID) {
+		this.flightID = flightID;
+	}
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+	public String getDepCity() {
+		return DepCity;
+	}
+
+	public void setDepCity(String DepCity) {
+		this.DepCity = DepCity;
+	}
+
+	public String getArrCity() {
+		return ArrCity;
+	}
+
+	public void setArrCity(String ArrCity) {
+		this.ArrCity = ArrCity;
+	}
+
+	public String getDepDate() {
+		return DepDate;
+	}
+
+	public void setDepDate(String DepDate) {
+		this.DepDate = DepDate;
+	}
+
+	public String getDepTime() {
+		return DepTime;
+	}
+
+	public void setDepTime(String DepTime) {
+		this.DepTime = DepTime;
+	}
+
+}
